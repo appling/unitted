16a7ddc60a79d7727fa04817849ac79bc10dd30b
Alison Appling <aappling@usgs.gov>
2017-07-21 11:18:05 -0700

    redo print.unitted_tbl_df for new tibble, dplyr

--- R/display.R
+++ R/display.R
@@ -96,24 +96,42 @@ setMethod(
 #' Print a unitted_tbl_df
 #' 
 #' @rdname print.unitted
-#' @param n Number of rows to show, as in \code{\link[tibble]{print.tbl_df}}
-#' @param width Width of text output to generate, as in
-#'   \code{\link[tibble]{print.tbl_df}}
-#' @importFrom dplyr dim_desc trunc_mat
-#' @import tibble
+#' @param n As in tibble's \code{\link[tibble]{formatting}}, Number of rows to
+#'   show. If `NULL`, the default, will print all rows if less than option 
+#'   `tibble.print_max`. Otherwise, will print `tibble.print_min` rows.
+#' @param width As in tibble's \code{\link[tibble]{formatting}}, Width of text
+#'   output to generate. This defaults to NULL, which means use 
+#'   `getOption("tibble.width")` or (if also NULL) `getOption("width")`; the 
+#'   latter displays only the columns that fit on one screen. You can also set 
+#'   `options(tibble.width = Inf)` to override this default and always print all
+#'   columns.
+#' @param n_extra As in tibble's \code{\link[tibble]{formatting}}, Number of
+#'   extra columns to print abbreviated information for, if the width is too
+#'   small for the entire tibble. If `NULL`, the default, will print information
+#'   about at most `tibble.max_extra_cols` extra columns.
 #' @examples
 #' tibble::as_tibble(u(list(x = u(1:500,"A"), y = u(runif(500),"B"), z = u(500:1,"C"))))
 setMethod(
   ".unitted_print", "tbl_df",
-  function(x, ..., n = NULL, width = NULL) {
-    # comparable to getAnywhere(print.tbl_df)
-    y <- trunc_mat(v(x), n = n, width = width)
-    # reformat the trunc_mat as unitted
-    for(colnm in names(y$table)) {
-      y$table[colnm] <- u(unclass(y$table[[colnm]]), get_units(x)[colnm])
-    }
-    y$table <- u(y$table)
-    print(y, n=n, width=width)
+  function(x, ..., n = NULL, width = NULL, n_extra = NULL) {
+    # shrink table as tibble does
+    y <- tibble::trunc_mat(v(x), n = n, width = width, n_extra = n_extra)
+    
+    # insert row for units
+    y$table <- y$table[c(1,1:nrow(y$table)),]
+    rownames(y$table)[1:2] <- c("U","")
+    y$table[1,] <- get_units(x)
+    
+    # reshrink table in case units are wider than other column contents
+    yu <- tibble::trunc_mat(y$table, n = n, width = width, n_extra = n_extra)
+    y$table <- y$table[names(yu$table)]
+    y[c('extra','width','n_extra')] <- yu[c('extra','width','n_extra')]
+    
+    # revise the summary vector element name (it's originally 'A tibble')
+    names(y$summary) <- 'A unitted tibble'
+    
+    # print
+    print(y, n=n, width=width, n_extra=n_extra)
     
     invisible(x)
   }
